5f4c31d623335d1a608a938cf904b8529c152374
Ceciliapradob <93287874+Ceciliapradob@users.noreply.github.com>
2021-11-25 22:00:55 -0300

    Add files via upload

---
+++ Homework realizado.js
@@ -0,0 +1,19 @@
+* Variables
+- Una variable es un elemento que se emplea para almacenar y hacer 
+referencia a otro valor. Se crean mediante la palabra reservada var. 
+
+* Strings 
+Cadena de caracteres. Se utiliza para representar datos textuales. 
+
+    * Funciones (argumentos, `return`) 
+    Arumentos. Es un objeto similar a Array accesible dentro de funciones
+    que contiene los valores de los argumentos pasados a esa funcion. 
+    Return; La sentencia return finaliza la ejecucion de la funcion y 
+    especifica un valor para ser devuelto a quien llama a la funcion. 
+
+    * Declaraciones `if` la sentencia IF es una estructura de control 
+    utilizada para tomar decisiones. Es un condicional que sirve 
+    para realizar unas u ottras operaciones en funcion de una expresion. 
+
+    * Valores booleanos (`true`, `false`) Representan un valor de verdad.
+    True o false.   
